c1fddfd392aaddf7d99eb04dba0dac3e4b31c65e
RC-Developer2022 <rcoimbra2022@outlook.com>
2024-06-18 12:45:23 -0300

    first commit

---
+++ src/main/java/course/coursejavacompleted/CourseJavaCompletedApplication.java
@@ -0,0 +1,13 @@
+package course.coursejavacompleted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseJavaCompletedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseJavaCompletedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/course/coursejavacompleted/CourseJavaCompletedApplicationTests.java
@@ -0,0 +1,13 @@
+package course.coursejavacompleted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseJavaCompletedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
